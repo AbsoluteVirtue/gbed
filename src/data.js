8abe07fe53db5c29d22f8bd0e0c39cc4ceec4db6
Ivan <3hitemouse@gmail.com>
2021-03-31 13:20:54 +0300

    tweaked heading margins

--- src/data.js
+++ src/data.js
@@ -4,17 +4,33 @@ text = `{
     "ou": "output:",
     "re": "return value:",
     "actor___tostring": {
-        "desc": "converts an actor class instance to a string",
+        "desc": "Converts an actor class instance to a string.",
         "params": "Actor (<b>userdata<\/b>) - instance of actor class"
     },
     "actor_AddTag": {
-        "desc": "adds a tag to the list of a given actor's tags",
+        "desc": "Adds a tag to the list of a given actor's tags.",
         "params": "\
         Actor (<b>userdata<\/b>) - instance of actor class<br>\
         Tag (<b>string<\/b>) - string to be added"
     },
     "actor_GetLocation": {
         "desc": "...",
+        "params": "Actor (<b>userdata<\/b>) - instance of actor class"
+    },
+    "actor_GetOverlaps": {
+        "desc": "...",
+        "params": "\
+        Actor (<b>userdata<\/b>) - instance of actor class<br>\
+        Class (<b>string<\/b>) - name of actor class"
+    },
+    "actor_GetTag": {
+        "desc": "Returns a tag at a given index from a list of a given actor's tags.",
+        "params": "\
+        Actor (<b>userdata<\/b>) - instance of actor class<br>\
+        Index (<b>number<\/b>) - index inside list of tags (integer value starting with 1)"
+    },
+    "actor_GetTags": {
+        "desc": "Returns a list of tags of a given actor.",
         "params": "Actor (<b>userdata<\/b>) - instance of actor class"
     }
 }`;
