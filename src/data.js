903fcadba4614b93e9fcd931297bfb6807cc89b9
Ivan <3hitemouse@gmail.com>
2021-03-31 12:26:40 +0300

    tweaked tags

--- src/data.js
+++ src/data.js
@@ -5,6 +5,12 @@ text = `{
     "re": "return value:",
     "actor___tostring": {
         "desc": "converts an actor class instance to a string",
-        "params": "Actor (userdata) - instance of actor class"
+        "params": "Actor (<b>userdata<\/b>) - instance of actor class"
+    },
+    "actor_AddTag": {
+        "desc": "adds a tag to the list of a given actor's tags",
+        "params": "\
+        Actor (<b>userdata<\/b>) - instance of actor class<br>\
+        Tag (<b>string<\/b>) - string to be added"
     }
 }`;
